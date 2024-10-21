ce44629430e10abd7d0b95cc38f5dd283c0ddb76
Yelshod 'fullstack dev <yelshodtoreyev@gmail.com>
2024-10-21 17:36:47 +0500

    test22ц3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
+  <input type="text" v-model="userName" placeholder="Name">
+  <input type="password" v-model="userPass" placeholder="Password">
+  <input type="email" v-model="userEmail" placeholder="Email">
+
 
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userPass: '',
+      userEmail: ''
+    }
+  },
+  methods: {}
+}
 
 </script>
 
