44e0006fdc8b901bb27e086232b6be117fbc40c7
Yelshod 'fullstack dev <yelshodtoreyev@gmail.com>
2024-10-23 17:23:11 +0500

    Components

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,68 @@
   <input type="text" v-model="userName" placeholder="Name">
   <input type="password" v-model="userPass" placeholder="Password">
   <input type="email" v-model="userEmail" placeholder="Email">
+  <p className = "error">{{ error }}</p>
+  <button @click="sendData()">Отправить</button>
 
+  <div v-if="users.length == 0" className="user">
+    У нас нет пользователей
+  </div>
+
+  <div v-else-if="users.length == 1" className="user">
+    Users has 1 element
+  </div>
+
+  <div v-else className="user">
+    Users has more than 1 element
+  </div>
+
+  <User v-for="(el,index) in users" :key="index" :user="el" :index = "index" :deleteUser="deleteUser"/>
 
 </template>
 
 <script>
+import User from "@/components/User.vue";
+
 export default {
+  components:  {User},
   data() {
     return {
+      users: [],
+      error: '',
       userName: '',
       userPass: '',
       userEmail: ''
     }
   },
-  methods: {}
+  methods: {
+    sendData() {
+      if (this.userName == '') {
+        this.error = 'Имя не введено';
+        return;
+      }
+      else if(this.userEmail == '') {
+        this.error = 'Email не введен'
+        return;
+      }
+      else if(this.userPass == '') {
+        this.error = 'Пароль не введен'
+        return;
+      }
+      this.error = '';
+
+
+      this.users.push({
+          name: this.userName,
+          pass: this.userPass,
+          email: this.userEmail
+      })
+    },
+
+    deleteUser(index) {
+          this.users.splice(index, 1);
+    }
+
+  }
 }
 
 </script>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,32 @@
 <template>
-
+  <div className="user">
+    <button @click="deleteUser(index)">Удалить</button>
+    <h3>{{ user.name }}</h3>
+    <p>{{user.email}} - <b>{{user.pass}}</b></p>
+  </div>
 </template>
 
 <script>
+import {toRef} from "vue";
 
+export default {
+  props: {
+      user: {
+        type: Object,
+        required: true
+      },
+      index: {
+        type: Object,
+        required: true
+      },
+      deleteUser: {
+        type: Function,
+        required: true
+      }
+  }
+}
 </script>
 
-<style scoped>
+<style>
 
 </style>
