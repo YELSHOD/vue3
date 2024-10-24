0ff380426976f8b2392ef17fd553de7ea7142299
Yelshod 'fullstack dev <yelshodtoreyev@gmail.com>
2024-10-24 15:43:45 +0500

    API OpenDataWeather

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,89 @@
-
-
 <script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      city: "",
+      error: "",
+      info: null
+    }
+  },
+  computed: {
+    cityName() {
+      return "<" + this.city + ">"
+    },
+    showTemp() {
+      return "Температура: " + this.info.main.temp
+    },
+    showFeelsLike() {
+      return "Ощущается как : " + this.info.main.feels_like
+    },
+    showMinTemp() {
+      return "Минимальная температура: " + this.info.main.temp_min
+    },
+    showMaxTemp() {
+      return "Максимальная температура: " + this.info.main.temp_max
+    }
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Нужное название более одного символа :)"
+          return false;
+      }
+
+      this.error = ""
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=50c854cb205e56e68eadf02d11e4527c`)
+          .then(res =>(this.info = res.data))
+    }
+
+  }
+}
 
 </script>
 
 <template>
-    <div class="wrapper">
-          <h1>Погодное приложение</h1>
-          <p>Узнать погоду в вашем городе</p>
-          <input type="text" placeholder="Введите город">
-          <button>Получить погоду</button>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в {{ city == "" ? "в вашем городе" : cityName }}</p>
+    <input type="text" v-model="city" placeholder="Введите город">
+    <button v-if="city !=''" @click="getWeather()">Получить погоду</button>
+    <button disabled v-else> Введите название города</button>
+    <p class="error">{{ error }}</p>
+
+    <div v-if="info != null">
+      <p>{{ showTemp }}</p>
+      <p>{{ showFeelsLike }}</p>
+      <p>{{ showMinTemp }}</p>
+      <p>{{ showMaxTemp }}</p>
     </div>
+  </div>
 </template>
 
 <style scoped>
-.wrapper {
-    width: 900px;
-    height: 500px;
-    border-radius: 50px;
-    background: #1f0f24 ;
-    text-align: center;
-    color: #fff;
+
+.error {
+  color: #cc1717;
 }
+
+.wrapper {
+  width: 900px;
+  height: 600px;
+  border-radius: 50px;
+  background: #1f0f24;
+  text-align: center;
+  color: #fff;
+}
+
 .wrapper h1 {
-  margin-top: 50px;
+  margin-top: 150px;
 
 }
 
 .wrapper p {
   margin-top: 20px;
+  font-size: 20px;
 }
 
 .wrapper input {
@@ -42,8 +96,15 @@
   padding: 5px 8px;
   outline: none;
 }
+
 .wrapper input:focus {
-  border-bottom-color: #6e2d7d ;
+  border-bottom-color: #6e2d7d;
+}
+
+
+.wrapper button:disabled {
+  background: #26ad38;
+  cursor: not-allowed;
 }
 
 .wrapper button {
